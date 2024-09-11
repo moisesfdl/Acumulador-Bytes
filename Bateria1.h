b7768ed1e368ab72ef90a690206af6bc71c80233
moises ferreira de lima <43420973+moisesfdl@users.noreply.github.com>
2024-09-10 23:12:56 -0300

    Create Bateria1.h
    esta em analise

---
+++ Bateria1.h
@@ -0,0 +1,115 @@
+#include <iostream>
+
+using namespace std;
+void Acumulador(){
+	long int byte;
+	float bit = 4;
+	int moisesfdl;
+	if(moisesfdl = true)
+	{
+		cout<<"| \Bytes : ";
+		for(int bit = 4 ; bit > 1; ++bit)
+		{
+			byte = bit + bit;
+			while(bit *+ 1)
+			{
+				bit + bit;
+				++bit;
+			}
+			cout<<byte;
+			//cout<<"\n";
+		}
+		//	return byte;
+	}
+	if(moisesfdl = true)
+	{
+		for(int bit = 4 ; bit > 1; ++bit)
+		{
+			byte = bit + bit;
+			while(bit *+ 1)
+			{
+				bit + bit;
+				++bit;
+			}
+			cout<<byte;
+			//cout<<"\n";
+		}
+		//	return byte;
+	}
+	if(moisesfdl = true)
+	{
+		for(int bit = 4 ; bit > 1; ++bit)
+		{
+			byte = bit + bit;
+			while(bit *+ 1)
+			{
+				bit + bit;
+				++bit;
+			}
+			cout<<byte;
+			//cout<<"\n";
+		}
+		//	return byte;
+	}
+		if(moisesfdl = true)
+	{
+		for(int bit = 4 ; bit > 1; ++bit)
+		{
+			byte = bit + bit;
+			while(bit *+ 1)
+			{
+				bit + bit;
+				++bit;
+			}
+			cout<<byte;
+			//cout<<"\n";
+		}
+		//	return byte;
+	}
+		if(moisesfdl = true)
+	{
+		for(int bit = 4 ; bit > 1; ++bit)
+		{
+			byte = bit + bit;
+			while(bit *+ 1)
+			{
+				bit + bit;
+				++bit;
+			}
+			cout<<byte;
+			//cout<<"\n";
+		}
+		//	return byte;
+	}
+		if(moisesfdl = true)
+	{
+		for(int bit = 4 ; bit > 1; ++bit)
+		{
+			byte = bit + bit;
+			while(bit *+ 1)
+			{
+				bit + bit;
+				++bit;
+			}
+			cout<<byte;
+			//cout<<"\n";
+		}
+		//	return byte;
+	}
+	if(moisesfdl = true)
+	{
+		for(int bit = 4 ; bit > 1; ++bit)
+		{
+			byte = bit + bit;
+			while(bit *+ 1)
+			{
+				bit + bit;
+				++bit;
+			}
+			cout<<byte;
+			//cout<<"\n";
+		}
+		//	return byte;
+	cout<<"\n";
+	}
+}
